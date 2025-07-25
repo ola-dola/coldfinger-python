d074f0dd6bc1f2744670e41954889a3c5f9add54
Ola Oredola <oredolaolamide@gmail.com>
2025-07-25 10:29:34 +0100

    ft: get files & dir info

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -1,21 +1,22 @@
 import os
 
 def get_files_info(working_directory, directory="."):
-  dir_path = os.path.join(working_directory, directory)
-  
-  if not os.path.exists(dir_path):
+  abs_working_directory = os.path.abspath(working_directory)
+  abs_target_directory = os.path.abspath(os.path.join(abs_working_directory, directory))
+    
+  if not abs_target_directory.startswith(abs_working_directory):
     return f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
   
-  if not os.path.isdir(dir_path):
+  if not os.path.isdir(abs_target_directory):
     return f'Error: "{directory}" is not a directory'
   
-  dir_contents = os.listdir(dir_path)
-  for item in dir_contents:
-    item_path = os.path.join(dir_path, item)
-    print(f"{item}: file_size={os.path.getsize(item_path)} bytes, is_dir={os.path.isdir(item_path)}")
+  def stringify_metadata(name):
+    content_path = os.path.join(abs_target_directory, name)
+    return f"{name}: file_size={os.path.getsize(content_path)} bytes, is_dir={os.path.isdir(content_path)}"
   
-
-if __name__ == '__main__':
-  print(get_files_info("calculator", "."))
-  
+  try:
+    contents = os.listdir(abs_target_directory)
+    return "\n".join(map(stringify_metadata, contents))
+  except Exception as err:
+    return f"Error listing files: {err}"
   
